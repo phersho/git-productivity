98080c1312d0e5946c76f485512459106d79d4de
Omar Del Rio <omar_delrio@msn.com>
2017-02-25 09:41:12 -0600

    Changed file in branch for conflict
    Added a second file in branch
    
    New code for feature #5

---
+++ SerializeableLinkedResource.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+using System.Net.Mime;
+
+namespace api.Core.Mailer
+{
+    [Serializable]
+    public class SerializeableLinkedResource
+    {
+        private readonly  String ContentId;
+        private readonly Uri ContentLink;
+        private readonly Stream ContentStream;
+        private readonly SerializeableContentType ContentType;
+        private readonly TransferEncoding TransferEncoding;
+
+        public SerializeableLinkedResource(LinkedResource linkedResource)
+        {
+            ContentId = linkedResource.ContentId;
+            ContentLink = linkedResource.ContentLink;
+            ContentType = new SerializeableContentType(linkedResource.ContentType);
+            TransferEncoding = linkedResource.TransferEncoding;
+
+            if (linkedResource.ContentStream != null)
+            {
+                var bytes = new byte[linkedResource.ContentStream.Length];
+                linkedResource.ContentStream.Read(bytes, 0, bytes.Length);
+                ContentStream = new MemoryStream(bytes);
+            }
+        }
+
+        public LinkedResource GetLinkedResource()
+        {
+            return new LinkedResource(ContentStream)
+                       {
+                           ContentId = ContentId,
+                           ContentLink = ContentLink,
+                           ContentType = ContentType.GetContentType(),
+                           TransferEncoding = TransferEncoding
+                       };
+        }
+    }
+}
---
+++ SerializeableMailAddress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace api.Core.Mailer
+{
+    [Serializable]
+    public class SerializeableMailAddress
+    {
+        private readonly  String User;
+        private readonly String Host;
+        private readonly String Address;
+        private readonly String DisplayName;
+
+        public SerializeableMailAddress(MailAddress address)
+        {
+            User = address.User;
+            Host = address.Host;
+            Address = address.Address;
+            DisplayName = address.DisplayName;
+        }
+
+        public MailAddress GetMailAddress()
+        {
+            return new MailAddress(Address, DisplayName);
+        }
+    }
+}
