9f40a0bd55cd28ce0d8a16a6a5c191e9d80827d5
Omar Del Rio <omar_delrio@msn.com>
2017-02-25 08:38:38 -0600

    Added code for feature #3

---
+++ User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace api.Core
+{
+    public class User:IIdentity
+    {
+        public enum IarUserTypes
+        {
+            Administrator = 0,
+            MasterAdmin,
+            Dispatcher,
+            User,
+            DispatcherUser,
+            Apparatus
+        }
+
+        public string Name { get; private set; }
+
+        public int MemberId { get; private set; }
+        public int SubscriberId { get; private set; }
+
+        public DateTime CreatedOn { get; private set; }
+
+        public string AuthenticationType { get; private set; }
+        public bool IsAuthenticated { get; private set; }
+
+        public IarUserTypes UserType { get; private set; }
+
+        public User(string name, int memberId, int subscriberId, DateTime createdOn, IarUserTypes userType, string authenticationType, bool isAuthenticated)
+        {
+            Name = name;
+            MemberId = memberId;
+            SubscriberId = subscriberId;
+            CreatedOn = createdOn;
+            AuthenticationType = authenticationType;
+            IsAuthenticated = isAuthenticated;
+
+            UserType = userType;
+        }
+    }
+}
---
+++ XmlWithDeclMediaTypeFormatter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace api.Core
+{
+    public class XmlWithDeclMediaTypeFormatter : XmlMediaTypeFormatter
+    {
+        public XmlWithDeclMediaTypeFormatter()
+        {
+            UseXmlSerializer = true;
+        }
+
+        public override Task WriteToStreamAsync(Type type, object value, Stream writeStream, HttpContent content,
+            TransportContext transportContext)
+        {
+            WriteXmlDeclaration(writeStream);
+            return base.WriteToStreamAsync(type, value, writeStream, content, transportContext);
+        }
+
+        private static void WriteXmlDeclaration(Stream writeStream)
+        {
+            const string XmlDeclaration = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+            var declBytes = Encoding.UTF8.GetBytes(XmlDeclaration);
+            writeStream.Write(declBytes, 0, declBytes.Length);
+        }
+    }
+}
